536557d6e9608927c17e615e3d13e9d9e0814a77
Manas <manasbeniwal0708@gmail.com>
2026-09-23 09:58:42 +0530

    Add Notes Adda plugin foundation

---
+++ notes-adda.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Notes Adda
+ * Description: Student notes sharing application.
+ * Version: 0.1.0
+ * Author: Manas
+ * Text Domain: notes-adda
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+define( 'NOTES_ADDA_VERSION', '0.1.0' );
+define( 'NOTES_ADDA_PATH', plugin_dir_path( __FILE__ ) );
+define( 'NOTES_ADDA_URL', plugin_dir_url( __FILE__ ) );
